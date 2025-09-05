148390330419e799fa3ae5bf41d4a92d47fea068
Shivangi1223 <singhshivangi21805@gmail.com>
2025-09-05 12:20:16 +0530

    Initial commit with README and one.cpp

---
+++ one.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    // cout<< "Value of n is: "<< n << endl;
+
+    if(n>0){
+        cout<<"A is positive"<<endl;
+    }else{
+        cout<<"A is negative"<<endl;
+    }
+
+    return 0;
+}
